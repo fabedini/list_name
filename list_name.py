5ef653aeefd609a8e7961a7ffe205a7678c657a0
fatemeh <48899733+fabedini@users.noreply.github.com>
2019-05-16 23:43:15 +0430

    Delete list_name.py

--- list_name.py
+++
@@ -1,8 +0,0 @@
-name=input('enter your name:')
-familly=input('enter your famili:')
-print=(name , familly)
-age=input('your age is:')
-if age <=10:
-   print('you are under age')
-else:
-   print("your age is iligal")
